80e65efa87b0d50772610b644133dfb1d7081803
Cort Fritz <cortfritz@me.com>
2014-05-30 10:43:10 -0700

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# altitude
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/TaskGroupController.js
@@ -0,0 +1,11 @@
+/**
+ * TaskGroupController
+ *
+ * @description :: Server-side logic for managing taskgroups
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
